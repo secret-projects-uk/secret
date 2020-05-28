884b2ddbd4cdf4da8dddea5eefd1885f3186bdae
Poltergeistv2 <alessio.potter6702@gmail.com>
2020-05-28 02:25:27 +0200

    Header, footer
    Started working on header and footer, idk why I added logo file since I used the one on bp website lol

---
+++ include/footer.php
@@ -0,0 +1,13 @@
+<?php ?>
+
+<html>
+
+    <link rel="stylesheet" type="text/css" href="footer.css">
+    
+    <footer id="Footer" >
+    
+        <h4>Hello</h4>
+        
+    </footer>
+    
+</html>
---
+++ include/header.php
@@ -0,0 +1,16 @@
+<?php ?>
+
+<html>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="header.css">
+
+    <header id="Header" >
+        
+        <a href="#Header" style="text-decoration:none"><h3 id="TopText" class="Name" >BP TEAM</h3></a>
+        <h3 id="TopText" class="About">ABOUT US</h3>
+        <h3 id="TopText" class="Products" >PRODUCTS</h3>
+        <a href="#Header"><img class="Logo" src="https://bpteam.xyz/assets/bplogo.png" ></a>
+</header>
+    
+</html>
