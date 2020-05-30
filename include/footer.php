be3c555924c00e54377107ec514c63daf7168630
Poltergeistv2 <alessio.potter6702@gmail.com>
2020-05-30 14:58:29 +0200

    A lot of stuff
    Added all icons
    Included everything in index
    Kept working on footer to add icons

---
+++ include/footer.php
@@ -0,0 +1,17 @@
+<?php ?>
+
+<html class="Noscrolly" >
+
+    <link rel="stylesheet" type="text/css" href="footer.css">
+    
+    <footer id="Footer" >
+    
+        <img class="DiscordLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Discord.png" >
+        <img class="YoutubeLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Youtube_svg_5279120.png" >
+        <img class="TwitterLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Twitter_svg_5279123.png" >
+        <img class="TwitchLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Twitch.png" >
+        <img class="DiscordLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Discord.png" >
+        
+    </footer>
+    
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+include('include/footer.php');
+include('include/header.php');
+
+?>
+
+<html  class="Noscrolly" >
+
+        <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="include/header.css">
+    <link rel="stylesheet" type="text/css" href="include/footer.css">
+
+    
+    <body>
+        <div id="IncludeContent" ></div>
+        <h2>HELLO THERE BLUEY SMELLS</h2>
+        
+    </body>
+    
+</html>
