7b46ac2c14b4f8db344ea65ab9c4a93f6877542e
Poltergeistv2 <alessio.potter6702@gmail.com>
2020-05-30 18:02:50 +0200

    Footer, Index
    Made some changes in footer and started working with main index page

--- index.php
+++ index.php
@@ -6,15 +6,47 @@ include('include/header.php');
 
 <html  class="Noscrolly" >
 
-        <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="include/header.css">
     <link rel="stylesheet" type="text/css" href="include/footer.css">
-
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+        <header id="Header" >
+        
+        <a href="#Header" style="text-decoration:none"><h3 style="text-decoration:none" id="TopText" class="Name" >BP TEAM</h3></a>
+        <h3 id="TopText" class="About">ABOUT US</h3>
+        <h3 id="TopText" class="Products" >PRODUCTS</h3>
+        <a href="#Header"><img class="Logo" src="https://bpteam.xyz/assets/bplogo.png" ></a>
+</header>
+    
+    
     
     <body>
-        <div id="IncludeContent" ></div>
-        <h2>HELLO THERE BLUEY SMELLS</h2>
+        
+        <div id="Top" >
+        
+        <div class="Textbox1">
+            <h2 class="Title4Box" >Who are we?</h2>
+            <h3 class="Text4Box" >We are the BP Team, we make mods for Minecraft and games under the name BP Studios. We started in January of 2019 and have grown our team to 6 since then,
+if you want to know about the team go to the about page and you can see who we are and what we wrote about our selves.
+Currently we are working on two Minecraft mods, World Plus and MiTrek, you can find more info on their product pages.</h3></div>
+        <img class="CodingPic" src="https://bpteam.xyz/secret/secret/test-website/assets/codingthenextgeneration.jpg" >
+        </div>
+            
+            
+        
         
     </body>
+    
+        <footer id="Footer" >
+    
+            <a href="https://discord.gg/Zawfp6K" ><img class="DiscordLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Discord.png" ></a>
+            <a href="https://www.youtube.com/channel/UC4MeGgjN60Spcmz7G7Gf9pQ" ><img class="YoutubeLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Youtube_svg_5279120.png" ></a>
+            <a href="https://twitter.com/BP_Team_" ><img class="TwitterLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Twitter_svg_5279123.png" ></a>
+            <a href="https://twitch.tv/bp_team_" ><img class="TwitchLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Twitch.png" ></a>
+            <a href="https://github.com/bpteammc" ><img class="GithubLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Github.png" ></a>
+        
+            <h3 style="letter-spacing: 30%" class="BottomText" >BP Team • 2019-2020</h3>
+    </footer>
     
 </html>
