0238811dd57a243cac21622de713c39f3a829128
Poltergeistv2 <alessio.potter6702@gmail.com>
2020-05-30 16:57:05 +0200

    Finished footer

--- include/footer.php
+++ include/footer.php
@@ -2,16 +2,18 @@
 
 <html class="Noscrolly" >
 
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="footer.css">
     
     <footer id="Footer" >
     
-        <img class="DiscordLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Discord.png" >
-        <img class="YoutubeLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Youtube_svg_5279120.png" >
-        <img class="TwitterLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Twitter_svg_5279123.png" >
-        <img class="TwitchLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Twitch.png" >
-        <img class="DiscordLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Discord.png" >
+            <a href="https://discord.gg/Zawfp6K" ><img class="DiscordLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Discord.png" ></a>
+            <a href="https://www.youtube.com/channel/UC4MeGgjN60Spcmz7G7Gf9pQ" ><img class="YoutubeLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Youtube_svg_5279120.png" ></a>
+            <a href="https://twitter.com/BP_Team_" ><img class="TwitterLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/iconfinder_Circled_Twitter_svg_5279123.png" ></a>
+            <a href="https://twitch.tv/bp_team_" ><img class="TwitchLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Twitch.png" ></a>
+            <a href="https://github.com/bpteammc" ><img class="GithubLogo" src="https://bpteam.xyz/secret/secret/test-website/assets/circled_Github.png" ></a>
         
+            <h3 style="letter-spacing: 30%" class="BottomText" >BP Team • 2019-2020</h3>
     </footer>
     
 </html>
